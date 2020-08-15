57f7ce93c83b8b36a70cbe724cb2d5d694a4acb0
msurio <suriomanthony@gmail.com>
2020-08-15 10:22:12 -0700

    my files

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ $(document).ready(function () {
     }
 
     $('#greeting').text(greet);
-
-    //Rendering Weather
+    
+    //weather function
     function renderWeather() {
         var city = 'san diego';
         var queryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=99adabcd9b9526ae2fc8e7bbc24f5de4';
@@ -35,11 +35,10 @@ $(document).ready(function () {
 
 
 
-    function renderDate() {
 
         var date = moment().format('M[/]DD[/]YYYY')
-        console.log(date);
-    };
+        $('.date').text(date);
+
 
 
 
@@ -53,3 +52,36 @@ $(document).ready(function () {
 
 
 })
+
+
+
+function renderDoSomething() {
+    var queryUrl = 'https://www.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true&key=AIzaSyDG00SsuoM_lFTEQpzWlbKSbkcJJUuyVKU';
+}
+
+
+
+renderWeather();
+//renderDate();
+
+
+
+   
+//campground API key ZX7CCMKAUBMGHEHP5TVW8QK7
+
+
+
+// YouTube API key AIzaSyDG00SsuoM_lFTEQpzWlbKSbkcJJUuyVKU
+// YouTube URL https://www.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true&key=AIzaSyDG00SsuoM_lFTEQpzWlbKSbkcJJUuyVKU
+
+// Amazon fitness 
+
+
+
+// YouTube API key AIzaSyDG00SsuoM_lFTEQpzWlbKSbkcJJUuyVKU
+// YouTube URL https://www.googleapis.com/youtube/v3/channels?part=contentDetails&mine=true&key=AIzaSyDG00SsuoM_lFTEQpzWlbKSbkcJJUuyVKU
+
+// Amazon fitness 
+
+
+// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment// Amazon API k//search url https://api.rainforestapi.com/request?api_key={OurAPIKey}_domain=amazon.com&type=search&search_term=fitness+equipment
